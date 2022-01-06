99c78bde11c603ab7b8d112387ee7bd4e3222312
Jin <JINWEN_SHIEH@ms.dlp.com.t>
2022-01-06 19:56:49 +0800

    [Feat] Mutiple list filter condition

--- TEST1/main.js
+++ TEST1/main.js
@@ -1,10 +1,14 @@
 const requestUrl =
   'https://run.mocky.io/v3/9d5f33ec-0d2f-4743-aac9-5aef32f7badf';
 var userList = [];
-var resultList = [];
+var filterKeys = {
+  name: '',
+  login_at: '',
+}
 
 window.onload = function () {
   initListFetch()
+  initSearch();
   backToTop();
 };
 
@@ -13,8 +17,15 @@ const initListFetch = () => {
     type: 'GET',
     url: requestUrl,
     dataType: 'json',
-    success: function (response) {
-      userList = response;
+    success: function (res) {
+      userList = res.map(user => {
+        return {
+          ...user,
+          name: `${user.first_name} ${user.last_name}`,
+          // 時間格式轉換 List: https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_tolocalestring_date_all
+          login_at: new Date(user.login_at).toLocaleString('sv-SE', {timeZone: 'Asia/Taipei'})
+        }
+      });
       insertHTML(listDOMGenerater(userList));
     },
     error: function (thrownError) {
@@ -23,38 +34,46 @@ const initListFetch = () => {
   });
 }
 
-const textSearch = () => {
-  $('#textSearch').on('input', e => {
-    const { value } = e.target;
-    resultList = userList.filter(user => (
-      `${user.first_name} ${user.last_name}`.toLowerCase()
-        .includes(value.toLowerCase().trim())
-    ));
-    insertHTML(listDOMGenerater(resultList));
-  });
+const initSearch = () => {
+  textSearch();
+  dateSearch();
 }
 
-const genderSearch = () => {
-  $('.search__field__genfer input').on('change', e => {
-    const { value } = e.target;
-    console.log(value);
-    // const resultList = userList.filter(user => (
-    //   user.login_at.toLowerCase()
-    //     .includes(value.toLowerCase().trim())
-    // ));
-    // console.log(resultList);
-    // insertHTML(listDOMGenerater(resultList));
+const textSearch = () => {
+  $('#textSearch').on('input', e => {
+    filterKeys.name = e.target.value.trim();
+    search();
   });
 }
 
 const dateSearch = () => {
-  $('#dateSearch').change(e => {
-    const { value } = e.target;
-    resultList = userList.filter(user => (
-      user.login_at.includes(value)
-    ));
-    insertHTML(listDOMGenerater(resultList));
+  const dateInput = $('#dateSearch');
+  $('#dateClear').click(e => {
+    e.preventDefault();
+    dateInput.val('');
+    filterKeys.login_at = '';
+    search();
+  })
+  dateInput.change(e => {
+    filterKeys.login_at = e.target.value;
+    search();
   });
+}
+
+const search = () => {
+  const resultList = userList.filter(user => {
+    return user.name.includes(filterKeys.name) && user.login_at.includes(filterKeys.login_at); 
+  })
+  // const resultList = userList.reduce((accu, curr) => {
+    // for (let key in filterKeys) {
+    //   if (!(curr[key].includes(filterKeys[key]))) {
+    //     break;
+    //   }
+    //   accu.push(curr);
+    // }
+    // return accu;
+  // }, []);
+  insertHTML(listDOMGenerater(resultList));
 }
 
 const insertHTML = (DOM) => {
@@ -69,7 +88,7 @@ const listDOMGenerater = (list) => {
     return `
       <div class="user__wrapper">
         <div id="${user.id}" class="user">
-          <h3>姓名: ${user.first_name} ${user.last_name}</h3>
+          <h3>姓名: ${user.name}</h3>
           <p>電子信箱: ${user.email}</p>
           <p>性別: ${user.gender}</p>
           <p>登入時間: ${user.login_at}</p>
